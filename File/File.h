6a03eba7750cd1fb1b77e7a721edf3e52be94e3a
christian otkjær <christian.otkjaer@gmail.com>
2016-03-04 11:28:55 +0100

    Initial Commit

---
+++ File/File.h
@@ -0,0 +1,19 @@
+//
+//  File.h
+//  File
+//
+//  Created by Christian Otkjær on 04/03/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for File.
+FOUNDATION_EXPORT double FileVersionNumber;
+
+//! Project version string for File.
+FOUNDATION_EXPORT const unsigned char FileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <File/PublicHeader.h>
+
+
