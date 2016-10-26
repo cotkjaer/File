271786e00e5ea0e361cb9abd47a08e0cdca02476
christian otkjær <christian.otkjaer@gmail.com>
2016-10-26 20:35:31 +0200

    Initial

---
+++ File/FileManager.swift
@@ -0,0 +1,61 @@
+//
+//  FileManager.swift
+//  File
+//
+//  Created by Christian Otkjær on 25/10/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+import Foundation
+
+//
+//  FileManager.swift
+//  SwiftPlus
+//
+//  Created by Christian Otkjær on 25/10/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+import Foundation
+
+public extension FileManager
+{
+    @discardableResult
+    func copy(resourceNamed name: String, ofType: String, inBundle bundle: Bundle = Bundle.main, toFolder folderPath: String) throws -> String?
+    {
+        let pathToFile = folderPath.appending("/\(name).\(ofType)")
+        
+        guard !FileManager.default.fileExists(atPath: pathToFile) else { return nil }
+        
+        guard let pathToBundledResource = bundle.path(forResource: name, ofType: ofType) else { return nil }
+        
+        try FileManager.default.copyItem(atPath: pathToBundledResource, toPath: pathToFile)
+        
+        return pathToFile
+    }
+    
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _ response: URLResponse?, _ error: Error? ) -> ())
+    {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func documentsFolderURL() -> URL?
+    {
+        guard let path = documentsFolderPath() else { return nil }
+        
+        return URL(fileURLWithPath: path, isDirectory: true)
+    }
+    
+    func documentURL(forFile fileName: String, fileExtension: String) -> URL?
+    {
+        return documentsFolderURL()?.appendingPathComponent(fileName).appendingPathExtension(fileExtension)
+    }
+    
+    func documentsFolderPath() -> String?
+    {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last
+    }
+}
